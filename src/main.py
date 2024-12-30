24abed1087acc590c6ae84d055ce6a818aefcdd3
Tushar Gupta <114675969+ummtushar@users.noreply.github.com>
2024-12-30 23:45:31 +0100

    updated link to download the scrubber dataset via one drive

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from datacleaner import DataCleaner
 
 df_gender = DataLoader("./data/gender.csv").load_data() 
 data_cleaner = DataCleaner(df_gender)
-df_gender = data_cleaner.scrubber(df_gender) #please note that this can take 10+ hours, for ease, I have uplodaded a file called df_geneder_augmented.csv that can be run directly under the link - https://tuenl-my.sharepoint.com/:x:/g/personal/t_gupta_student_tue_nl/EXvzzx2mKPBIo1j_x_BCMeMBGdDdSoZeo2vpGlZtbeODPg?e=MwdNws 
+df_gender = data_cleaner.scrubber(df_gender) #please note that this can take 10+ hours, for ease, I have uplodaded a file called df_geneder_augmented.csv that can be run directly under the link - https://tuenl-my.sharepoint.com/:f:/g/personal/t_gupta_student_tue_nl/EqyqKsxeZDNFkCR-Zf29FDoBMK6eZpjRwFfOzRJecfYWQg?e=QLJUE8
 df_gender = data_cleaner.gender_swap(df_gender)
 
 # df_gender = pd.read_csv("./data/df_gender_augmented.csv") 
